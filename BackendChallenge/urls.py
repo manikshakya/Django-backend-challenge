949f12eb2d5af93d92e348bb28f3c3cd46478354
manikshakya <hotpop_manik1@yahoo.com>
2019-08-08 02:06:20 +0100

    Homepage fixed

--- BackendChallenge/urls.py
+++ BackendChallenge/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('learning.urls')),
     path('admin/', admin.site.urls),
     path('learning/', include('learning.urls')),
 ]
